bcabb3e18545820b3c0abc760ba9757dd95829ac
Erick Edgardo Cerna Requejo <william8178@hotmail.com>
2021-01-29 19:53:29 -0500

    Clases y validaciones

--- script/questic.js
+++ script/questic.js
@@ -1,66 +1,34 @@
 /*Para almacenar las misiones*/
+const quest = new Quest();
 const dataQuest = [];
 const questContainerPrimary = document.getElementById("quest-container-principal");//questContainer principal
 const questContainerSecondary = document.getElementById("quest-container-secondary");//questContainer secundario
-
-/*--------Objeto Quest------*/
-function Quest (content, priority, nObj, qstatus) {
-	this.content = content; 
-	if (priority === true) {
-		this.priority = priority;
-	}else {
-		this.priority = false;
-	}
-
-	if (nObj === "0") {
-		nObj = "";
-		this.nObj = nObj;
-	}
-	else {
-		this.nObj = nObj;
-	}
-	this.qstatus = qstatus;
-}
-/*----------------------------------------------------*/
+let alert = document.getElementById("alert"); // leer div alerta
+let msg;
 
 //Carga del documento
 const init = () => {
 	document.querySelector("#btn-add").addEventListener("click", get, false);
 }
+
 //Obtener el value del input
-const get = () => {
+function get() {
+
 	const qstContent = document.getElementById("qstContent");
 	const nObjectives = document.getElementById("nObjectives");
 	const qstPriority = document.getElementById("qstPrincipal");
-	dataQuest.push(new Quest (qstContent.value,qstPriority.checked,nObjectives.value,"ongoing"));
-	addElement();
+
+	if (qstContent.value === '') {
+		quest.getMsg(alert, "No olvides registrar tu misión");
+	} else if (nObjectives.value < 0) {
+		quest.getMsg(alert, "Los objetivos no son negativos");
+	} else {
+		document.getElementById("alert").hidden = true;
+		dataQuest.push(new Quest(qstContent.value, qstPriority.checked, nObjectives.value, "ongoing"));
+		quest.addElement(dataQuest);
+	}
+
 }
 
-const addElement = () => {
 
-	let var1 = dataQuest[dataQuest.length-1];//Para almacenar el ultimo objeto Quest
-
-	const divRow = document.createElement("div");
-	divRow.className = "row"; // div Fila
-
-	var1.priority
-	? questContainerPrimary.insertAdjacentElement("beforeend", divRow)//true
-	: questContainerSecondary.insertAdjacentElement("beforeend", divRow);//false
-
-	const divContent = document.createElement("div");
-	divContent.className = "col-lg-6"
-	divContent.textContent = `${var1.content}`;
-	divRow.insertAdjacentElement('beforeend', divContent);
-
-	if (var1.nObj != "") {
-		const divObj = document.createElement("div");
-		divObj.className = "col-lg-3";
-		divObj.textContent = `(0/${var1.nObj})`
-		divRow.insertAdjacentElement("beforeend",divObj);
-	}
-	const checkIco = document.createElement("i");
-			checkIco.className = `bi-check-square-fill checkIco`;
-			divRow.insertAdjacentElement("afterbegin",checkIco);
-		}
-
-		init();
+init();
